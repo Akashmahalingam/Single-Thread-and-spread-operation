c9ff54546035ddcfcc8219ebe36c0388dc65e26e
Akash <akbnytech@gmail.com>
2024-11-20 13:51:55 +0530

    Add files via upload

---
+++ js/single.js
@@ -0,0 +1,8 @@
+// Single Thread Oparation
+	document.write('Arun'); 
+	
+	setTimeout(() => { 
+		document.write('Ram'); 
+	}, 3000); 
+		
+	document.log('Kavin'); 
---
+++ js/spread.js
@@ -0,0 +1,14 @@
+// Array Combine
+const vehicles_buying = ["car", "bike", "van"];
+const additional_buying = ["omni", "bus"];
+
+// Spread Operator
+let buying =[...vehicles_buying, 15, ...additional_buying];
+console.log(buying);
+document.write(buying);
+alert(buying);
+
+
+
+ 
+
